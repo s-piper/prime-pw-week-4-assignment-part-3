c19f0e87ac10ea4fdfb0c296d2675355883514c2
Stephen Piper <stephen.r.piper@gmail.com>
2021-04-24 22:15:04 -0500

    intial commit. created var basket and addItem function

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -1,3 +1,8 @@
 console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
+let basket = [];
+
+function addItem() {
+
+}
