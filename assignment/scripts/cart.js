bd068763104ab060eb721fdbc21af70f4d2e297a
Stephen Piper <stephen.r.piper@gmail.com>
2021-04-24 22:30:57 -0500

    Completed first function by adding banana, orange, grapes to basket. Returned true.

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -3,6 +3,13 @@ console.log('***** Cart Functions *****');
 // We want to see how you are testing your code!!!
 let basket = [];
 
-function addItem() {
-
-}
+function addItem(nameOfItem) {
+  console.log('in addItem', nameOfItem);
+  //push new items into basket array.
+  basket.push(nameOfItem);
+  return true;
+} //end addItem
+addItem ('banana');
+addItem ('orange');
+addItem ('grapes');
+console.log(`Added 3 items, should return true: ${addItem()}`);
